3c881b7e51b7ded3fd2f58eeda6eb49797c96301
azizsur <azizsurya79@gmail.com>
2026-05-07 22:34:38 +0700

    Remove production URL from CORS configuration

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const cors = require("cors");
 
 // Izinkan origin frontend lokal yang umum dipakai saat development
 app.use(cors({
-  origin: ['http://localhost', 'http://localhost:5173', 'http://127.0.0.1:5500', "https://refined-cortex-442704-m0.et.r.appspot.com"],
+  origin: ['http://localhost', 'http://localhost:5173', 'http://127.0.0.1:5500',],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
